78f5b509e436f5a5716abc6057b76dabfc4dbc1d
halanm <halanm123@gmail.com>
2024-04-06 15:46:33 -0700

    Added get_generator_by_name on Generator object

--- models/data/generator.py
+++ models/data/generator.py
@@ -1,11 +1,16 @@
 class Generator:
     generators = []
-
+ 
     def __init__(self, name, category, prompt):
         self.name = name
         self.category = category
         self.prompt = prompt
         Generator.generators.append(self)
-
+ 
     def list_generators_by_category(category):
         return [model for model in Generator.generators if model.category == category]
+ 
+    def get_generator_by_name(name):
+        for generator in Generator.generators:
+            if generator.name == name:
+                return generator
